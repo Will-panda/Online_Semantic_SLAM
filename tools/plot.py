f414ff8a61918dd2f210a18829dfa0a265c94c4a
liwenqiang <2402870298@qq.com>
2019-05-12 21:30:44 +0800

    add plot tool

---
+++ tools/plot.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib as plt
+import sys
+
+path = sys.argv
+a = np.loadtxt('result/')
+
+
+plt.plot()
+plt.xlabel('长度')
+plt.ylabel('Translation Error')
+plt.show()
